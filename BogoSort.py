68eb545ff7264415974554f9ce0435fdcf17338e
Shaneron <77985354+Unigmos@users.noreply.github.com>
2021-09-16 19:11:40 +0900

    Update BogoSort.py

--- BogoSort.py
+++ BogoSort.py
@@ -4,8 +4,7 @@ a = [3, 5, 2, 1, 4]
 
 shuffle_data = random.shuffle(a)
 while True:
-    if a == sorted(a):
-        print(a)
-        break
     random.shuffle(a)
     print(a)
+    if a == sorted(a):
+        break
