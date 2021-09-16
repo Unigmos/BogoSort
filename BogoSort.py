4b3376bef8f2456ee95b5c3e25bde8eab5f1030a
Shaneron <77985354+Unigmos@users.noreply.github.com>
2021-09-16 19:17:47 +0900

    Update BogoSort.py

--- BogoSort.py
+++ BogoSort.py
@@ -1,10 +1,16 @@
 import random
 
 a = [3, 5, 2, 1, 4]
+counter = 0
 
 shuffle_data = random.shuffle(a)
+counter += 1
+
 while True:
     random.shuffle(a)
+    counter += 1
     print(a)
     if a == sorted(a):
         break
+
+print("試行回数" + str(counter) + "回")
